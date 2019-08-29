f459f22f3969dffd50b1cf1f686a4b2dd749ae79
ivan <Ivan_ldp@163.com>
2019-08-29 22:17:17 +0800

    删除src里的文件

--- src/main/java/cn/kgc/controller/Test.java
+++
@@ -1,7 +0,0 @@
-package cn.kgc.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class Test {
-}
--- src/main/java/cn/kgc/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.kgc.dao;
-
-import cn.kgc.pojo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
